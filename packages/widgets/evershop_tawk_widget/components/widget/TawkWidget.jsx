86d22fc18813ff7d8c320b813961efffc1159109
huangj1e <10999@88.com>
2024-09-26 17:37:48 +0800

    修改正则

--- packages/widgets/evershop_tawk_widget/components/widget/TawkWidget.jsx
+++ packages/widgets/evershop_tawk_widget/components/widget/TawkWidget.jsx
@@ -6,15 +6,21 @@ import PropTypes from 'prop-types';
 export default function TawkWidget({ tawkWidget: { text } }) {
 
   let src = extractSrcValue(text);
+
   useEffect(() => {
     if (src) {
-      const script = document.createElement('script');
-      script.src = src;
-      script.async = true;
-      document.body.appendChild(script);
+      var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+      (function () {
+        var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+        s1.async = true;
+        s1.src = text;
+        s1.charset = 'UTF-8';
+        s1.setAttribute('crossorigin', '*');
+        s0.parentNode.insertBefore(s1, s0);
+      })();
       return () => {
         // 卸载组件时移除脚本
-        document.body.removeChild(script);
+        document.body.removeChild(s1);
       };
     }
   }, []);
@@ -46,14 +52,14 @@ export const variables = `{
 
 
 /**
- * 从 <script> 标签中提取 src 的值
+ * 从 <script> 标签中提取 链接 的值
  * @param {string} scriptTag - 包含 <script> 标签的字符串
  * @returns {string|null} - 返回 src 的值，如果未匹配则返回 null
  */
 function extractSrcValue(scriptTag) {
   // 使用正则表达式匹配并提取 src 的值（带引号和不带引号两种情况）
-  const match = scriptTag.match(/src=\\"([^"]+)\\"/) || scriptTag.match(/src="([^ ]+)"/);
+  const match = scriptTag.match(/https[^']+/)
 
   // 如果匹配成功，返回 src 的值；否则返回 null
-  return match ? match[1] : null;
+  return match ? match[0] : null;
 }
